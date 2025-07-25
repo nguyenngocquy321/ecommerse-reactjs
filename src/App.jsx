453182da0838a97bd7c606ac99d7285b04f18baf
Nguyễn Ngọc Quý <nguyenngocquy321@gmail.com>
2025-07-25 17:02:12 +0700

    update'

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import MyFooter from '@components/Footer/Footer';
 import MyHeader from '@components/Header/Header';
 import MainLayout from '@components/Layout/Layout';
-import MyButton from './components/Button/Button';
-
 function App() {
     return (
         <>
             <MainLayout>
-                {/* <MyHeader />
-                Content
+                <MyHeader />
+                {/* Content
                 <MyFooter /> */}
-                <MyButton />
             </MainLayout>
         </>
     );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,25 @@
+import BoxIcon from './BoxIcon/BoxIcon';
+import { dataBoxIcon } from './constants';
 function MyHeader() {
-    return <div>MY HEADER</div>;
+    return (
+        <div>
+            <div>
+                <div>
+                    {dataBoxIcon.map((item, index) => {
+                        return (
+                            <BoxIcon
+                                type={item.type}
+                                href={item.href}
+                                key={index}
+                            />
+                        );
+                    })}
+                </div>
+                <div></div>
+            </div>
+            <div></div>
+            <div></div>
+        </div>
+    );
 }
-
 export default MyHeader;
